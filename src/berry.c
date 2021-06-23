9087fd8014f050de83eb528c3c9d002a39feb39a
TheXaman <maxvdl@web.de>
2021-06-23 11:54:52 +0200

    fixed old song name and include for no watering berries on rainy routes

--- src/berry.c
+++ src/berry.c
@@ -14,6 +14,7 @@
 #include "constants/berry.h"
 #include "constants/event_object_movement.h"
 #include "constants/items.h"
+#include "constants/map_groups.h"
 
 static u32 GetEnigmaBerryChecksum(struct EnigmaBerry *enigmaBerry);
 static bool32 BerryTreeGrow(struct BerryTree *tree);
